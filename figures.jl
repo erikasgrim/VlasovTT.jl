f159bde52cde61fea53051f26eb56847b89cc1d7
ermas3 <erik.asgrim@gmail.com>
2025-12-15 08:27:20 +0100

    plot ke

--- figures.jl
+++ figures.jl
@@ -5,6 +5,8 @@ function read_data(filepath::String)
     times = Float64[]
     charges = Float64[]
     ef_energy = Float64[]
+    kinetic_energy = Float64[]
+    total_energy = Float64[]
     open(filepath, "r") do io
         header = readline(io)  # Skip header
         for line in eachline(io)
@@ -13,20 +15,22 @@ function read_data(filepath::String)
             push!(times, parse(Float64, cols[2]))
             push!(charges, parse(Float64, cols[3]))
             push!(ef_energy, parse(Float64, cols[4]))
+            push!(kinetic_energy, parse(Float64, cols[5]))
+            push!(total_energy, parse(Float64, cols[6]))
         end
     end
-    return steps, times, charges, ef_energy
+    return steps, times, charges, ef_energy, kinetic_energy, total_energy
 end
 
 let 
     filepath = "results/two_stream/data.csv"
-    steps, times, charges, ef_energy = read_data(filepath)
+    steps, times, charges, ef_energy, kinetic_energy, total_energy = read_data(filepath)
 
     println(steps)
 
     plt = plot(
         times,
-        ef_energy,
+        [ef_energy, kinetic_energy, total_energy],
         xlabel = "Time",
         ylabel = "Total Charge",
         title = "Total Charge vs Time",
