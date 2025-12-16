a4f4fa01c0f287003973a12ada89a58e6cdf49f2
ermas3 <erik.asgrim@gmail.com>
2025-12-16 08:29:43 +0100

    ef separate rank and fourier solver

--- src/operators/poisson.jl
+++ src/operators/poisson.jl
@@ -48,8 +48,8 @@ function get_charge_density(psi::MPS; dv::Real=1.0)
     end
 
     odd_tensors = [working[i] for i in 1:N if isodd(i)]
-    odd_tensors[1] *= dv
-    return MPS(odd_tensors)
+    #odd_tensors[1] *= dv
+    return MPS(odd_tensors) * dv
 end
 
 function ones_mps(sites)
@@ -91,23 +91,81 @@ function ones_mps(sites)
     return MPS(tensors)
 end
 
+struct PoissonCache
+    id_mps::MPS
+end
+
+function build_poisson_cache(psi_mps::MPS)
+    sites_cd_mps = siteinds(psi_mps)
+    id_mps = ones_mps(sites_cd_mps)
+    return PoissonCache(id_mps)
+end
+
 function get_electric_field_mps(
     psi_mps::MPS,
     full_poisson_mpo::TCI.TensorTrain;
     dv::Real=1.0,
-    tolerance=1e-8,
-    maxrank=nothing,
+    cutoff=1e-8,
+    maxrank_ef=16,
     alg="naive",
+    cache::Union{PoissonCache,Nothing}=nothing,
 )
-    cd_mps = get_charge_density(psi_mps; dv)
+    cd_mps = get_charge_density(psi_mps; dv) # Electron density
 
-    id_mps = ones_mps(siteinds(cd_mps)) # TODO: To not create a new MPS each time, pass in an identity MPS
-    cd_mps = id_mps - cd_mps
+    # Plot charge charge density.
+    id_mps = isnothing(cache) ? ones_mps(siteinds(cd_mps)) : cache.id_mps
+    cd_mps = id_mps - cd_mps # Charge density
 
     sites_cd_mps = siteinds(cd_mps)
     sites_poisson_mpo = [[prime(s,1), s] for s in sites_cd_mps]
     full_poisson_mpo_it = MPO(full_poisson_mpo; sites=sites_poisson_mpo)
-    electric_field_mps = apply(full_poisson_mpo_it, cd_mps; alg=alg, truncate=true, maxdim=maxrank, cutoff=tolerance)
+    electric_field_mps = apply(full_poisson_mpo_it, cd_mps; alg=alg, maxdim=maxrank_ef, cutoff=cutoff)
 
-    return electric_field_mps
+    return cd_mps, electric_field_mps
+end
+
+function get_charge_density_kv(psi::MPS; dv::Real=1.0)
+    N = length(psi)
+    N == 0 && return psi
+
+    working = copy(ITensors.cpu(psi))
+    for i in 2:2:N
+        si = siteind(working, i)
+        zero_mode_vec = ITensor(si)
+        zero_mode_vec[si => 1] = 1.0
+
+        Ai = working[i] * zero_mode_vec
+        if i < N
+            working[i + 1] = Ai * working[i + 1]
+        else
+            working[i - 1] = working[i - 1] * Ai
+        end
+        working[i] = ITensor(1.0)
+    end
+
+    odd_tensors = [working[i] for i in 1:N if isodd(i)]
+    #odd_tensors[1] *= dv
+    return MPS(odd_tensors) * dv
+end
+
+function get_electric_field_mps_kv(
+    psi_mps::MPS,
+    full_poisson_mpo::TCI.TensorTrain;
+    dv::Real=1.0,
+    cutoff=1e-8,
+    maxrank_ef=16,
+    alg="naive",
+    cache::Union{PoissonCache,Nothing}=nothing,
+)
+    cd_mps = get_charge_density_kv(psi_mps; dv) # Electron density
+
+    id_mps = isnothing(cache) ? ones_mps(siteinds(cd_mps)) : cache.id_mps
+    cd_mps = id_mps - cd_mps # Charge density
+
+    sites_cd_mps = siteinds(cd_mps)
+    sites_poisson_mpo = [[prime(s,1), s] for s in sites_cd_mps]
+    full_poisson_mpo_it = MPO(full_poisson_mpo; sites=sites_poisson_mpo)
+    electric_field_mps = apply(full_poisson_mpo_it, cd_mps; alg=alg, maxdim=maxrank_ef, cutoff=cutoff)
+
+    return cd_mps, electric_field_mps
 end
