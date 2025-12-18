248ca7eafab8cfe23e1f52b125a4aeb959fbc44b
ermas3 <erik.asgrim@gmail.com>
2025-12-18 12:14:20 +0100

    correct params

--- examples/two_stream_unfiltered_TCI.jl
+++ examples/two_stream_unfiltered_TCI.jl
@@ -13,31 +13,31 @@ using CUDA
 using Plots
 using ProgressBars
 
-function run_two_stream(; use_gpu::Bool = false, save_every::Int = 10)
+function run_two_stream(; use_gpu::Bool = true, save_every::Int = 10)
 
     # Simulation parameters
     dt = .1
     Tfinal = 60.0
     nsteps = Int(Tfinal / dt)
-    k_cut = 2^7 # This keeps the 2^... lowest negative AND positive modes. 
+    k_cut = 2^8 # This keeps the 2^... lowest negative AND positive modes. 
     beta = 2.0
 
-    simulation_name = "two_stream_unfiltered_TCI_v1"
+    simulation_name = "two_stream_unfiltered_TCI_v4"
     
     # Grid parameters
     R = 10
 
-    xmin = -5.0
-    xmax = 5.0
-    vmin = -1.2
-    vmax = 1.2
+    xmin = -pi / 3.06
+    xmax = pi / 3.06
+    vmin = -0.5
+    vmax = 0.5
     normalize = true
 
     # TT parameters
-    TCI_tolerance = 1e-6
-    maxrank = 160
-    maxrank_ef = 8
-    cutoff = 1e-8
+    TCI_tolerance = 1e-8
+    maxrank = 128
+    maxrank_ef = 12
+    cutoff = 1e-7
 
     # Build phase space grids and simulation parameters
     phase = PhaseSpaceGrids(R, xmin, xmax, vmin, vmax)
@@ -76,8 +76,8 @@ function run_two_stream(; use_gpu::Bool = false, save_every::Int = 10)
         lsb_first = true,
     )
 
-    # Build initial condition
-    ic_fn = two_stream_instability_ic(phase; A = 0.1, v0 = .5, vt = 0.2, mode = 3)
+    # Build initial pivots around +- v0 in velocity space
+    ic_fn = two_stream_instability_ic(phase; A = 0.1, v0 = .2, vt = 0.01, mode = 1)
     tt, _, _ = build_initial_tt(ic_fn, R; tolerance = params.tolerance)
     println("Initial condition TT ranks: ", TCI.rank(tt))
 
@@ -129,26 +129,22 @@ function run_two_stream(; use_gpu::Bool = false, save_every::Int = 10)
             #target_norm = init_charge,
         )
 
-        if step % save_every == 0 || step == 1 || step == nsteps
-            n_digits = 6
-            elapsed_time = round(time() - loop_start_time; digits = n_digits)
-            
-            # Copy the MPS and bring it back to v-space for visualization
-            psi_plot = copy(psi_mps)
-            psi_plot = apply(itensor_mpos.v_inv_fourier, psi_plot; alg = params.alg, maxdim = maxrank, cutoff = params.cutoff)
-            #psi_plot = apply(itensor_mpos.x_inv_fourier, psi_plot; alg = params.alg, maxdim = maxrank, cutoff = params.cutoff)
-            
-            write_data(
-                step, 
-                round(step * params.dt, digits=n_digits), 
-                round(real(total_charge(psi_plot, phase; cache=observables_cache)), digits=n_digits),
-                round(real(electric_field_energy(ef_mps, phase)), digits=n_digits),
-                round(real(kinetic_energy(psi_plot, phase; cache=observables_cache)), digits=n_digits),
-                maxlinkdim(psi_mps),
-                elapsed_time,
-                simulation_dir
-            )
+        n_digits = 9
+        elapsed_time = round(time() - loop_start_time; digits = n_digits)
+        psi_plot = copy(psi_mps)
+        psi_plot = apply(itensor_mpos.v_inv_fourier, psi_plot; alg = params.alg, maxdim = maxrank, cutoff = params.cutoff)
+        @time write_data(
+            step, 
+            round(step * params.dt, digits=n_digits), 
+            round(real(total_charge(psi_plot, phase; cache=observables_cache)), digits=n_digits),
+            round(real(electric_field_energy(ef_mps, phase)), digits=n_digits),
+            round(real(kinetic_energy(psi_plot, phase; cache=observables_cache)), digits=n_digits),
+            maxlinkdim(psi_mps),
+            elapsed_time,
+            simulation_dir
+        )
 
+        if step % save_every == 0 || step == 1 || step == nsteps
             tt_snapshot = TCI.TensorTrain(ITensors.cpu(psi_plot))
             f_vals = [tt_snapshot(origcoord_to_quantics(phase.x_v_grid, (x, v))) for v in v_vals, x in x_vals]
             Plots.savefig(
