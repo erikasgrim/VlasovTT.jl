c4ff391680c90409f21c700e312a0221dae16fd0
ermas3 <erik.asgrim@gmail.com>
2025-12-19 13:46:20 +0100

    cleanup code

--- src/solver.jl
+++ src/solver.jl
@@ -9,9 +9,6 @@ struct SolverMPOs
     full_free_streaming_fourier_mpo::TCI.TensorTrain
     poisson_mpo::TCI.TensorTrain
     full_poisson_mpo::TCI.TensorTrain
-    stretched_kv_mpo::TCI.TensorTrain
-    diffusion_mpo::TCI.TensorTrain
-    half_filtering_mpo::TCI.TensorTrain
 end
 
 function build_fourier_mpos(R::Int; tolerance::Real = 1e-8, lsb_first::Bool = false)
@@ -34,33 +31,8 @@ function build_fourier_mpos(R::Int; tolerance::Real = 1e-8, lsb_first::Bool = fa
     )
 end
 
-# function build_kv_mpo(Lv, M, kv_grid; tolerance::Real = 1e-12, k_cut::Real = 2^8, beta::Real = 2.0)
-#     function kv_kernel(kv)
-#         q_bits_normal = origcoord_to_quantics(kv_grid, kv)
-#         q_bits_reversed = reverse(q_bits_normal)
-#         kv_reversed = quantics_to_origcoord(kv_grid, q_bits_reversed)
-
-#         nv = k_to_n(kv_reversed, M)
-
-#         return 2pi * nv / Lv * frequency_filter(nv; k_cut = k_cut, beta = beta)
-#     end
-
-#     initial_pivots = [origcoord_to_quantics(kv_grid, n_to_k(n, M)) for n in -10:10]
-
-#     qtci, _, _ = quanticscrossinterpolate(
-#         Float64,
-#         kv_kernel,
-#         kv_grid,
-#         initial_pivots;
-#         tolerance = tolerance,
-#     )
-#     return tt_to_mpo(TCI.TensorTrain(qtci.tci))
-# end
-
 function build_kv_mpo(Lv, M, kv_grid; tolerance::Real = 1e-12, k_cut::Real = 2^8, beta::Real = 2.0)
     function kv_kernel(q_bits)
-        #q_bits_normal = origcoord_to_quantics(kv_grid, kv)
-        #q_bits_reversed = reverse(q_bits_normal)
         kv_reversed = quantics_to_origcoord(kv_grid, reverse(q_bits))
 
         nv = k_to_n(kv_reversed, M)
@@ -84,8 +56,6 @@ end
 
 function build_diffusion_mpo(dt, Lv, M, kv_grid; tolerance::Real = 1e-12, nu::Real = 3e-5, k_cut::Real = 2^8, beta::Real = 2.0)
     function kv_kernel(q_bits)
-        #q_bits_normal = origcoord_to_quantics(kv_grid, kv)
-        #q_bits_reversed = reverse(q_bits_normal)
         kv = quantics_to_origcoord(kv_grid, reverse(q_bits))
 
         nv = k_to_n(kv, M)
@@ -174,29 +144,6 @@ function build_solver_mpos(
         lsb_first = lsb_first,
     )
 
-    diffusion_mpo = build_diffusion_mpo(
-        dt,
-        phase.Lv,
-        phase.M,
-        phase.kv_grid;
-        tolerance = tolerance,
-        k_cut = k_cut,
-        beta = beta,
-    )
-
-    diffusion_mpo = stretched_mpo(diffusion_mpo, 2, 2)
-
-    half_filtering_mpo = build_filtering_mpo(
-        dt / 2,
-        phase.Lv,
-        phase.Lx,
-        phase.M,
-        phase.kv_grid,
-        phase.kx_grid;
-        tolerance = tolerance,
-        v0 = v0,
-    )
-
     poisson_mpo = get_poisson_mpo(
         phase.Lx,
         phase.M,
@@ -217,17 +164,6 @@ function build_solver_mpos(
         tolerance = tolerance,
     )
 
-    kv_mpo = build_kv_mpo(
-        phase.Lv, 
-        phase.M, 
-        phase.kv_grid; 
-        tolerance = tolerance,
-        k_cut = k_cut,
-        beta = beta,
-    )
-
-    stretched_kv_mpo = stretched_mpo(kv_mpo, 2, 2)
-
     return SolverMPOs(
         fourier.x_fourier_mpo,
         fourier.v_fourier_mpo,
@@ -239,9 +175,6 @@ function build_solver_mpos(
         full_free_streaming_fourier_mpo,
         poisson_mpo,
         full_poisson_mpo,
-        stretched_kv_mpo,
-        diffusion_mpo,
-        half_filtering_mpo,
     )
 end
 
@@ -254,17 +187,13 @@ function prepare_itensor_mpos(
     full_free_stream_fourier_mpo_it = MPO(mpos.full_free_streaming_fourier_mpo; sites = sites_mpo)
     half_free_stream_fourier_mpo_it = MPO(mpos.half_free_streaming_fourier_mpo; sites = sites_mpo)
 
+    fourier_mpo_it = MPO(mpos.fourier_mpo; sites = sites_mpo)
+
     x_fourier_mpo_it = MPO(mpos.x_fourier_mpo; sites = sites_mpo)
     x_inv_fourier_mpo_it = MPO(mpos.x_inv_fourier_mpo; sites = sites_mpo)
 
     v_fourier_mpo_it = MPO(mpos.v_fourier_mpo; sites = sites_mpo)
     v_inv_fourier_mpo_it = MPO(mpos.v_inv_fourier_mpo; sites = sites_mpo)
-
-    stretched_kv_mpo_it = MPO(mpos.stretched_kv_mpo; sites = sites_mpo)
-
-    diffusion_mpo_it = MPO(mpos.diffusion_mpo; sites = sites_mpo)
-    half_filtering_mpo_it = MPO(mpos.half_filtering_mpo; sites = sites_mpo)
-    #mixed_basis_free_stream_mpo_it = MPO(mpos.mixed_basis_free_streaming_mpo; sites = sites_mpo)
 
     full_free_stream_mpo_it = apply(
         x_inv_fourier_mpo_it,
@@ -302,12 +231,6 @@ function prepare_itensor_mpos(
 
         v_fourier_mpo_it = cu(v_fourier_mpo_it)
         v_inv_fourier_mpo_it = cu(v_inv_fourier_mpo_it)
-
-        stretched_kv_mpo_it = cu(stretched_kv_mpo_it)
-
-        diffusion_mpo_it = cu(diffusion_mpo_it)
-        half_filtering_mpo_it = cu(half_filtering_mpo_it)
-        #mixed_basis_free_stream_mpo_it = cu(mixed_basis_free_stream_mpo_it)
     end
 
     return (
@@ -319,9 +242,5 @@ function prepare_itensor_mpos(
         x_inv_fourier = x_inv_fourier_mpo_it,
         v_fourier = v_fourier_mpo_it,
         v_inv_fourier = v_inv_fourier_mpo_it,
-        stretched_kv = stretched_kv_mpo_it,
-        diffusion = diffusion_mpo_it,
-        half_filtering = half_filtering_mpo_it,
-        #mixed_basis_free_stream = mixed_basis_free_stream_mpo_it,
     )
 end
