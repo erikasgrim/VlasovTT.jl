5feca1d4a1c1316a54d0837157b0d1f4ad78d1d8
ermas3 <erik.asgrim@gmail.com>
2025-12-19 10:15:10 +0100

    fit two stream to linear

--- plot_two_stream.jl
+++ plot_two_stream.jl
@@ -24,21 +24,23 @@ function read_data(filepath::String)
 end
 
 let 
-    directory_path = "results/two_stream_unfiltered_TCI_v4/"
+    directory_path = "results/two_stream_unfiltered_TCI_v10/"
     filepath = joinpath(directory_path, "data.csv")
     steps, times, charges, ef_energy, kinetic_energy, total_energy = read_data(filepath)
 
     # Extract the indices of local maximima of the electric field energy
 
 
-    # Linear fit     # ef_max_indices = findall(i -> (i > 1 && i < length(ef_energy)) && (ef_energy[i] > ef_energy[i-1]) && (ef_energy[i] > ef_energy[i+1]), 1:length(ef_energy))
-    # ef_max_times = times[ef_max_indices]
-    # log_ef_max_values = log.(ef_energy[ef_max_indices])
-    # matrix = hcat(2 .* ef_max_times, ones(length(ef_max_times)))
-    # fit = matrix \ log_ef_max_values
-    # ef_fitted = exp(fit[2]) .* exp.(2 * fit[1] .* times)
+    # Linear fit     
+    ef_indices = findall(i -> (times[i] > 10.0) && (times[i] < 20), 1:length(ef_energy))
+    ef_times = times[ef_indices]
+    log_ef_values = log.(ef_energy[ef_indices])
+    matrix = hcat(2 .* ef_times, ones(length(ef_times)))
+    fit = matrix \ log_ef_values
+    ef_fitted = exp(fit[2]) .* exp.(2 * fit[1] .* times)
+
     gamma_analytic = 0.35355
-    ef_analytic = [1e-5 * exp(2 * gamma_analytic * t) for t in times]
+    ef_analytic = [exp(fit[2]) .* exp(2 * gamma_analytic * t) for t in times]
 
     default(
         fontfamily = "Computer Modern",
@@ -50,12 +52,13 @@ let
 
     plt = plot(
         times,
-        [ef_energy, ef_analytic],
+        [ef_energy, ef_analytic, ef_fitted],
         xlabel = L"t\ [\omega_{pe}^{-1}]",
         ylabel = L"\mathcal{E}_E",
-        label = L"\mathrm{Numerical}",
+        label = [L"\mathrm{Numerical}" L"\mathrm{Analytic\ (\gamma = 0.3535)}" L"\mathrm{Fitted\ (\gamma=}" * latexstring(round(fit[1]; digits=4)) * L")"],
         legend = :bottomright,
-        linestyles = :solid,
+        linestyles = [:solid :dash :dot],
+        color = [1 2 :black],
         yaxis = :log10,
         framestyle = :box,
     )
